5a2f0b078b8d9b4959de39a68f0cdd90b70c0479
rockpell <aythffk@gmail.com>
2022-01-08 01:25:17 +0900

    Feat: nest directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentModule } from './comment/comment.module';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
+  imports: [CommentModule, UserModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
