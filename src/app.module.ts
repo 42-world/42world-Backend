cc453e219e805b2f858924946b98526f60900137
rockpell <aythffk@gmail.com>
2022-01-08 02:06:59 +0900

    Feat: postgresql docker-compose 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentModule } from './comment/comment.module';
@@ -6,7 +8,21 @@ import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [CommentModule, UserModule, ArticleModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 2345,
+      username: 'ft_world',
+      password: 'ft_world',
+      database: 'ft_world',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    }),
+    CommentModule,
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
