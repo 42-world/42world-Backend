928a610ff86c394b8c0847168cbd493ab28afdc9
rockpell <aythffk@gmail.com>
2022-01-08 02:07:19 +0900

    Feat: user 기본 entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
+  nickname!: string;
+
+  @Column({ type: 'varchar', length: 140, nullable: false })
+  password!: string;
+
+  @Column({ type: 'varchar', length: 140, nullable: false })
+  email!: string;
+
+  @Column({ type: 'varchar', length: 140, nullable: false })
+  last_login!: string;
+
+  @Column({ nullable: false, default: false })
+  is_active!: boolean;
+
+  @Column({ type: 'varchar', length: 140, nullable: false })
+  role!: string;
+
+  @Column({ type: 'varchar', length: 140, nullable: false })
+  picture!: string;
+}
