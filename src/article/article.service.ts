c6adac01c0cc9c0d987ea3d726ef26a18e84683c
rockpell <aythffk@gmail.com>
2022-01-11 00:37:06 +0900

    Feat: article find add api 추가
    - article을 category에 따라 가져오는 get api 추가
    - categoryName 없이 api를 호출하면 모든 목록 가져옴

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+
+import { ArticleRepository } from './repositories/article.repository';
+import { FindAllArticleDto } from './dto/find-all-article.dto';
 
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
-  }
+  constructor(private readonly articleRepository: ArticleRepository) {}
 
-  findAll() {
-    return `This action returns all article`;
+  findAll(options?: FindAllArticleDto) {
+    return this.articleRepository.findAll(options);
   }
 
   findOne(id: number) {
     return `This action returns a #${id} article`;
-  }
-
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} article`;
   }
 }
---
+++ src/article/dto/find-all-article.dto.ts
@@ -0,0 +1,3 @@
+export class FindAllArticleDto {
+  readonly categoryName?: string;
+}
