e284a55e74bfafee8c9eea936dff04c0e021862a
huni <petercha2000@gmail.com>
2022-01-13 00:30:43 +0900

    Refactor: 주석 제거

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -14,18 +14,10 @@ import { UpdateArticleDto } from './dto/update-article.dto';
 import { FindAllArticleDto } from './dto/find-all-article.dto';
 import { Article } from './entities/article.entity';
 
-/**
- * 제가 아직 nest 에 안익숙해서 모든 함수에 return type 을 명시했습니다...
- * 마찬가지로 경로도 명시적으로 표시해뒀습니다...
- */
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
-  /**\
-   * 존재하지 않는 writer나 category 를 넣어도 잘 들어가네요
-   * validate 를 해야할거같아요
-   */
   @Post()
   create(@Body() createArticleDto: CreateArticleDto): Promise<Article> {
     return this.articleService.create(createArticleDto);
