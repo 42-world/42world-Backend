fa9c3aa08fa58e070b9994c5978a4206bae88eca
huni <petercha2000@gmail.com>
2022-01-15 00:58:23 +0900

    Feat: comment entity 추가

--- src/best/entities/best.entity.ts
+++ src/best/entities/best.entity.ts
@@ -1,1 +1,34 @@
-export class Best {}
+import { User } from '@user/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  Index,
+} from 'typeorm';
+
+@Entity('best')
+export class Best {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ nullable: false })
+  @Index('ix_user_id')
+  user_id!: number;
+
+  @ManyToOne(() => User, (user) => user.best, {
+    createForeignKeyConstraints: false,
+    nullable: false,
+  })
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  user?: User;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+}
