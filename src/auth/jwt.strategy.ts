4aebc95716ba8a7b818c8fc80c34bec82f402c61
Luna <luvbliny@gmail.com>
2022-01-16 21:09:55 +0900

    Refactor: type applied to parameter jwt payload

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ACCESS_TOKEN } from './constants/access-token';
+import { JWTPayload } from './interfaces/jwt-payload.interface';
 
 const getAccessToken = (request: any): string => {
   return request.cookies[ACCESS_TOKEN];
@@ -19,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: JWTPayload) {
     const user = await this.userService.findOne(payload.userId);
     if (!user) {
       throw new UnauthorizedException();
