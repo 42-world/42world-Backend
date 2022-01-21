d838d9db9e12db235f2d391c76b00eb242593be7
huni <petercha2000@gmail.com>
2022-01-21 17:53:27 +0900

    Docs: auth swagger 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,20 @@ import { Controller, Get, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 import { UserService } from '@user/user.service';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './jwt-auth.guard';
 import { GithubAuthGuard } from './github-auth.guard';
 import { JWTPayload } from './interfaces/jwt-payload.interface';
 import { GithubProfile } from './interfaces/github-profile.interface';
 import { ACCESS_TOKEN } from './constants/access-token';
 import { GetGithubProfile, Public } from './auth.decorator';
+import {
+  ApiCookieAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -19,6 +26,13 @@ export class AuthController {
   @Get('github')
   @Public()
   @UseGuards(GithubAuthGuard)
+  @ApiOperation({
+    summary: '깃허브 로그인',
+    description: `
+      로그인이 되어있지 않은경우, 깃허브 로그인 페이지로 이동합니다
+      깃허브 로그인이 끝나면 지정된 프론트 페이지로 이동합니다.`,
+  })
+  @ApiOkResponse({ description: '깃허브 페이지' })
   githubLogin(): void {
     console.log('send to login page');
     return;
@@ -27,6 +41,14 @@ export class AuthController {
   @Get('github/callback')
   @Public()
   @UseGuards(GithubAuthGuard)
+  @ApiOperation({
+    summary: '깃허브 로그인 콜백',
+    description: `
+      깃허브 로그인이 끝나고 호출해주세요.
+      깃허브 로그인이 끝나지 않았다면,
+      다시 깃허브 로그인 페이지로 이동합니다.`,
+  })
+  @ApiOkResponse({ description: '로그인 성공' })
   async githubCallback(
     @GetGithubProfile() githubProfile: GithubProfile,
     @Res({ passthrough: true }) response: Response,
@@ -40,7 +62,10 @@ export class AuthController {
   }
 
   @Get('signout')
-  @UseGuards(JwtAuthGuard)
+  @ApiCookieAuth()
+  @ApiUnauthorizedResponse({ description: '인증 실패' })
+  @ApiOperation({ summary: '로그아웃' })
+  @ApiOkResponse({ description: '로그아웃 성공' })
   signout(@Res({ passthrough: true }) response: Response): void {
     response.clearCookie(ACCESS_TOKEN);
   }
