746552d9d4cfba359a50c1852035585798604d6f
huni <petercha2000@gmail.com>
2022-01-22 01:16:11 +0900

    Fix: seed data CamelCase로 수정

--- src/database/seeder/article/data.ts
+++ src/database/seeder/article/data.ts
@@ -3,8 +3,8 @@ import { Article } from '@root/article/entities/article.entity';
 
 export class SeederDataArticle extends PartialType(Article) {
   id: number;
-  category_id?: number;
-  writer_id?: number;
+  categoryId?: number;
+  writerId?: number;
 }
 
 export const articles: SeederDataArticle[] = [
@@ -12,21 +12,21 @@ export const articles: SeederDataArticle[] = [
     id: 1,
     title: 'title1',
     content: 'haha',
-    category_id: 1,
-    writer_id: 1,
+    categoryId: 1,
+    writerId: 1,
   },
   {
     id: 2,
     title: 'title2',
     content: 'haha haha',
-    category_id: 1,
-    writer_id: 2,
+    categoryId: 1,
+    writerId: 2,
   },
   {
     id: 3,
     title: 'title3',
     content: 'haha haha haha',
-    category_id: 2,
-    writer_id: 1,
+    categoryId: 2,
+    writerId: 1,
   },
 ];
