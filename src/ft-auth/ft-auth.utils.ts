6f3c3b8eeceeb25cda9ab06fcc25b42b75cd08c3
Luna <luvbliny@gmail.com>
2022-01-22 19:23:06 +0900

    Refactor: ttl 24 hours to 30 minutes

--- src/ft-auth/ft-auth.utils.ts
+++ src/ft-auth/ft-auth.utils.ts
@@ -2,7 +2,8 @@ import * as bcrypt from 'bcryptjs';
 
 export const TITLE = '[42WORLD] 이메일 인증을 완료하세요';
 export const EMAIL = 'student.42seoul.kr';
-export const TIME2LIVE = 86400;
+const MINUTE = 60;
+export const TIME2LIVE = 30 * MINUTE;
 
 export const getNickname = (email: string) => email.split('@')[0];
 export const getEmail = (nickname: string) => `${nickname}@${EMAIL}`;
