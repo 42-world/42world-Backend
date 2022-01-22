8e25765dd41a4c3fb1c59d9858fa5a3c70b625f4
rockpell <aythffk@gmail.com>
2022-01-23 04:48:33 +0900

    Feat: e2e user test cookie 적용 완료

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,24 +1,20 @@
 import { SeederModule } from './../src/database/seeder/seeder.module';
-import { UserRepository } from './../src/user/repositories/user.repository';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { ConfigModule } from '@nestjs/config';
 import { getConnection } from 'typeorm';
 import * as cookieParser from 'cookie-parser';
-import { APP_GUARD, NestFactory } from '@nestjs/core';
+import { APP_GUARD } from '@nestjs/core';
 
 import { JwtAuthGuard } from './../src/auth/jwt-auth.guard';
 import { AuthModule } from './../src/auth/auth.module';
-import { ormconfig } from './../src/database/ormconfig';
-import { getEnvPath } from './../src/utils';
-import { DatabaseModule } from './../src/database/database.module';
 import { UserModule } from './../src/user/user.module';
 import { EntityNotFoundExceptionFilter } from './../src/filters/entity-not-found-exception.filter';
 import { Seeder } from '@root/database/seeder/seeder';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
+  let JWT;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -48,17 +44,24 @@ describe('UserController (e2e)', () => {
     await seeder.seed();
 
     app = app.getHttpServer();
+    JWT = process.env.TEST_JWT;
   });
 
   it('/users/ (GET)', async () => {
-    const response = await request(app).get('/users');
-    expect(response.status).toEqual(401);
+    const response = await request(app)
+      .get('/users')
+      .set('Cookie', `access_token=${JWT}`);
+
+    expect(response.status).toEqual(200);
     return;
   });
 
   it('/users/:id (GET)', async () => {
-    const response = await request(app).get('/users/1');
-    expect(response.status).toEqual(401);
+    const response = await request(app)
+      .get('/users/1')
+      .set('Cookie', `access_token=${JWT}`);
+
+    expect(response.status).toEqual(200);
     return;
   });
 
