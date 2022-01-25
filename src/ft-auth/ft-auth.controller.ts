bcf05e39d5cdd2fa151554ed3c92f9ed4a905298
huni <petercha2000@gmail.com>
2022-01-26 03:10:52 +0900

    Feat: sendMail 에서 intraId 를 DTO로 이동
    - validate를 위해서, DTO로 이동
    - 기존 DTO 삭제
    - singin-ft-auth DTO 추가

--- src/ft-auth/ft-auth.controller.ts
+++ src/ft-auth/ft-auth.controller.ts
@@ -2,13 +2,14 @@ import { Body, Controller, Post, Get, Query } from '@nestjs/common';
 import { FtAuthService } from './ft-auth.service';
 import { GetUser } from '@root/auth/auth.decorator';
 import { User } from '@root/user/entities/user.entity';
+import { SigninFtAuthDto } from './dto/signin-ft-auth.dto';
 
 @Controller('ft-auth')
 export class FtAuthController {
   constructor(private readonly ftAuthService: FtAuthService) {}
 
   @Post()
-  async sendMail(@GetUser() user: User, @Body('intraId') intraId: string) {
+  async sendMail(@GetUser() user: User, @Body() { intraId }: SigninFtAuthDto) {
     await this.ftAuthService.signin(intraId, user);
   }
 
