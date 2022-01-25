1feb158a620d57f989abf8b128343497b079df82
huni <petercha2000@gmail.com>
2022-01-26 05:36:42 +0900

    Feat: best 에 article join

--- src/best/repositories/best.repository.ts
+++ src/best/repositories/best.repository.ts
@@ -5,9 +5,14 @@ import { Best } from '../entities/best.entity';
 @EntityRepository(Best)
 export class BestRepository extends Repository<Best> {
   async findAll(options: FindAllBestDto): Promise<Best[]> {
-    const query = this.createQueryBuilder('best');
+    const query = this.createQueryBuilder('best').leftJoinAndSelect(
+      'best.article',
+      'article',
+    );
 
-    if (options.limit) query.limit(options.limit);
+    if (options.limit) {
+      query.limit(options.limit);
+    }
 
     return query.getMany();
   }
