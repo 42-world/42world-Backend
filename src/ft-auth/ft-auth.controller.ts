1e98ab37052befff4964f78c5bdcdb5a240da26a
huni <petercha2000@gmail.com>
2022-01-26 03:13:45 +0900

    Feat: sendMail에 Novice 권한, getCode에 Public 권한 추가
    - 이메일 인증은 Novice도 가능하도록 수정
    - 코드를 받을땐, 서로다른 브라우저상에서 쿠키값이 없는 상태로 인증할수있기 때문에, Public으로 변경
    - Public 으로 변경하여도, 문제가없는지 검토 필요

--- src/ft-auth/ft-auth.controller.ts
+++ src/ft-auth/ft-auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, Get, Query } from '@nestjs/common';
 import { FtAuthService } from './ft-auth.service';
-import { GetUser } from '@root/auth/auth.decorator';
+import { GetUser, Novice, Public } from '@root/auth/auth.decorator';
 import { User } from '@root/user/entities/user.entity';
 import { SigninFtAuthDto } from './dto/signin-ft-auth.dto';
 
@@ -9,11 +9,13 @@ export class FtAuthController {
   constructor(private readonly ftAuthService: FtAuthService) {}
 
   @Post()
+  @Novice()
   async sendMail(@GetUser() user: User, @Body() { intraId }: SigninFtAuthDto) {
     await this.ftAuthService.signin(intraId, user);
   }
 
   @Get()
+  @Public() // TODO: check this
   async getAuthCode(@Query('code') code: string) {
     await this.ftAuthService.getAuth(code);
   }
