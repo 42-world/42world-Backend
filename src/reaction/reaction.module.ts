f62631b2ca0e4e2062434b6193f95c0b35fff482
blingblin-g <luvbliny@gmail.com>
2022-01-27 01:56:19 +0900

    Feat: 게시글, 댓글 좋아요 기능 추가
    Co-authored-by: rockpell  <aythffk@gmail.com>

--- src/reaction/reaction.module.ts
+++ src/reaction/reaction.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReactionService } from './reaction.service';
 import { ReactionController } from './reaction.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReactionArticle } from './entities/reaction-article.entity';
+import { ReactionComment } from './entities/reaction-comment.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReactionArticle, ReactionComment])],
   controllers: [ReactionController],
   providers: [ReactionService],
 })
