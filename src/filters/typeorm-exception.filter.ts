90a59912f0aba4ecba134aaa1be8e52849494847
rockpell <aythffk@gmail.com>
2022-01-29 22:14:43 +0900

    Feat: db 에러나면 slack으로 에러메시지 전송

--- src/filters/typeorm-exception.filter.ts
+++ src/filters/typeorm-exception.filter.ts
@@ -2,6 +2,7 @@ import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
 import { Response } from 'express';
 import { TypeORMError } from 'typeorm';
 import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
+import axios from 'axios';
 
 const FIND_DOUBLE_QUOTE = /\"/g;
 
@@ -10,6 +11,16 @@ export class TypeormExceptionFilter implements ExceptionFilter {
   public catch(exception: TypeORMError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+
+    const PHASE = process.env.NODE_ENV;
+    const slackMessage = `[${PHASE}] ${exception.name}: ${exception.message}}`;
+
+    axios.post(
+      'https://hooks.slack.com/services/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',
+      { text: slackMessage },
+    );
+
+    console.error(exception);
 
     if (exception instanceof EntityNotFoundError) {
       return response.status(404).json({
