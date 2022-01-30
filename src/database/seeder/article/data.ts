d2d657ef1d89c62eabc3676ae74b0bc62c1d801a
rockpell <aythffk@gmail.com>
2022-01-30 12:54:08 +0900

    Feat: 마이그레이션 기능 부활, 기본 카테고리 추가
    - 마이그레이션 기능 다시 활성화
    - 기본적으로 들어가있어야하는 카테고리 3개 마이그레이션에 포함해서 추가
    - article seeder data에서 id 제거

--- src/database/seeder/article/data.ts
+++ src/database/seeder/article/data.ts
@@ -2,29 +2,55 @@ import { PartialType } from '@nestjs/mapped-types';
 import { Article } from '@root/article/entities/article.entity';
 
 export class SeederDataArticle extends PartialType(Article) {
-  id: number;
   categoryId?: number;
   writerId?: number;
 }
 
 export const articles: SeederDataArticle[] = [
   {
-    id: 1,
     title: 'title1',
     content: 'haha',
     categoryId: 1,
     writerId: 1,
   },
   {
-    id: 2,
     title: 'title2',
     content: 'haha haha',
     categoryId: 1,
     writerId: 2,
   },
   {
-    id: 3,
     title: 'title3',
+    content: 'haha haha haha',
+    categoryId: 2,
+    writerId: 1,
+  },
+  {
+    title: 'title4',
+    content: 'haha haha haha',
+    categoryId: 2,
+    writerId: 1,
+  },
+  {
+    title: 'title5',
+    content: 'haha haha haha',
+    categoryId: 2,
+    writerId: 1,
+  },
+  {
+    title: 'title6',
+    content: 'haha haha haha',
+    categoryId: 2,
+    writerId: 1,
+  },
+  {
+    title: 'title7',
+    content: 'haha haha haha',
+    categoryId: 2,
+    writerId: 1,
+  },
+  {
+    title: 'title8',
     content: 'haha haha haha',
     categoryId: 2,
     writerId: 1,
