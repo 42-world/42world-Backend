2c03d5ba5c3571b4db337a3188bf2e5afa997422
rockpell <aythffk@gmail.com>
2022-02-09 22:35:15 +0900

    Fix: all exception filter, http adapter 제거
    - http adapter는 express 외의 다른 프레임워크와의 호환을 위해 사용하는건데 우리는 굳이 필요없어서 제거

--- src/filters/all-exception.filter.ts
+++ src/filters/all-exception.filter.ts
@@ -1,39 +1,22 @@
-import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
-import { HttpAdapterHost } from '@nestjs/core';
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
 import { INTERNAL_ERROR_MESSAGE } from '@root/filters/constant';
 import { errorHook } from '@root/utils';
+import { Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
-
-  catch(
-    exception: { stack: string; message: string },
-    host: ArgumentsHost,
-  ): void {
-    const { httpAdapter } = this.httpAdapterHost;
-
+  catch(exception: { stack: string; message: string }, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-
-    const httpStatus =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    const responseBody = {
-      statusCode: httpStatus,
-      message: INTERNAL_ERROR_MESSAGE,
-    };
+    const response = ctx.getResponse<Response>();
 
     console.error(exception);
     errorHook('Unknown Error', exception.message);
 
-    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+    return response.status(500).json({
+      message: {
+        statusCode: 500,
+        message: INTERNAL_ERROR_MESSAGE,
+      },
+    });
   }
 }
