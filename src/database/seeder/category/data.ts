f35ab7afac8bfd4bf4dbd0b40b61a54f3b550aab
huni <petercha2000@gmail.com>
2022-02-14 19:22:25 +0900

    Feat: 카테고리 seed 수정

--- src/database/seeder/category/data.ts
+++ src/database/seeder/category/data.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { Category } from '@category/entities/category.entity';
+import { UserRole } from '@root/user/interfaces/userrole.interface';
 
 export class SeederDataCategory extends PartialType(Category) {
   id: number;
@@ -10,9 +11,31 @@ export const categories: SeederDataCategory[] = [
   {
     id: 1,
     name: '자유게시판',
+    writableArticle: UserRole.CADET,
+    readonlyArticle: UserRole.CADET,
+    writableComment: UserRole.CADET,
+    readonlyComment: UserRole.CADET,
+    reactionable: UserRole.CADET,
+    anonymity: false,
   },
   {
     id: 2,
-    name: '자유게시판2',
+    name: '익명게시판',
+    writableArticle: UserRole.CADET,
+    readonlyArticle: UserRole.CADET,
+    writableComment: UserRole.CADET,
+    readonlyComment: UserRole.CADET,
+    reactionable: UserRole.CADET,
+    anonymity: true,
+  },
+  {
+    id: 3,
+    name: '42born2code 공지',
+    writableArticle: UserRole.ADMIN,
+    readonlyArticle: UserRole.CADET,
+    writableComment: UserRole.ADMIN,
+    readonlyComment: UserRole.ADMIN,
+    reactionable: UserRole.ADMIN,
+    anonymity: false,
   },
 ];
