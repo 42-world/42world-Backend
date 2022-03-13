30b44346809aca7706c33cf85bd471ec8c3fc3ca
huni <petercha2000@gmail.com>
2022-03-14 00:00:49 +0900

    Fix: seed 적용시, 필수 컬럼이 없어서 에러나는것 수정

--- src/database/seeder/user/data.ts
+++ src/database/seeder/user/data.ts
@@ -13,12 +13,16 @@ export const users: SeederDataUser[] = [
   {
     id: 1,
     oauthToken: 'test1234',
+    githubUsername: 'string1',
+    githubUid: 'string2',
     nickname: 'first user',
     role: UserRole.CADET,
   },
   {
     id: 2,
     oauthToken: 'test2345',
+    githubUsername: 'string2',
+    githubUid: 'string3',
     nickname: 'second user',
     role: UserRole.NOVICE,
   },
