903d6e5e22f79308463f24b661f1bf43eb08960f
huni <petercha2000@gmail.com>
2022-03-15 01:07:39 +0900

    Refactor: 카테고리 엔티티에서 swgger 제거
    - base category 에 swagger가 적용되어있으므로 제거

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,5 +1,4 @@
 import { Article } from '@article/entities/article.entity';
-import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@user/interfaces/userrole.interface';
 import {
   Entity,
@@ -14,47 +13,36 @@ import {
 
 @Entity('category')
 export class Category {
-  @ApiProperty()
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @ApiProperty()
   @Column({ type: 'varchar', length: 40, nullable: false })
   name!: string;
 
-  @ApiProperty({ example: UserRole.CADET })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.CADET })
   writableArticle!: string;
 
-  @ApiProperty({ example: UserRole.CADET })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.CADET })
   readableArticle!: string;
 
-  @ApiProperty({ example: UserRole.CADET })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.CADET })
   writableComment!: string;
 
-  @ApiProperty({ example: UserRole.CADET })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.CADET })
   readableComment!: string;
 
-  @ApiProperty({ example: UserRole.CADET })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.CADET })
   reactionable!: string;
 
-  @ApiProperty()
   @Column({ nullable: false, default: false })
   anonymity!: boolean;
 
-  @ApiProperty()
   @CreateDateColumn({ type: 'timestamp' })
   createdAt!: Date;
 
-  @ApiProperty()
   @UpdateDateColumn({ type: 'timestamp' })
   updatedAt!: Date;
 
-  @ApiProperty()
   @DeleteDateColumn({ type: 'timestamp' })
   @Index('ix_deleted_at')
   deletedAt?: Date;
