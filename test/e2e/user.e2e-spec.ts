5d85d3e3d7ed641b57d7a683df16998cdba6e084
SeongMin Park <parkseongmin98@gmail.com>
2022-03-27 22:43:11 +0900

    Feat: 유저 테스트 케이스 추가

--- test/e2e/user.e2e-spec.ts
+++ test/e2e/user.e2e-spec.ts
@@ -1,30 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
-import { getConnection } from 'typeorm';
-import * as cookieParser from 'cookie-parser';
-
-import { AuthModule } from '@auth/auth.module';
-import { UserModule } from '@user/user.module';
-import { UserRepository } from '@user/repositories/user.repository';
-import { JWTPayload } from '@auth/interfaces/jwt-payload.interface';
-import { AuthService } from '@auth/auth.service';
-import { TestBaseModule } from './test.base.module';
-import { TypeormExceptionFilter } from '@root/filters/typeorm-exception.filter';
 import { ArticleModule } from '@article/article.module';
-import { ArticleRepository } from '@article/repositories/article.repository';
 import { Article } from '@article/entities/article.entity';
-import { UpdateUserProfileRequestDto } from '@root/user/dto/request/update-user-profile-request.dto';
+import { ArticleRepository } from '@article/repositories/article.repository';
+import { AuthModule } from '@auth/auth.module';
+import { AuthService } from '@auth/auth.service';
+import { JWTPayload } from '@auth/interfaces/jwt-payload.interface';
 import { CategoryModule } from '@category/category.module';
 import { CategoryRepository } from '@category/repositories/category.repository';
-import { Comment } from '@comment/entities/comment.entity';
 import { CommentModule } from '@comment/comment.module';
+import { Comment } from '@comment/entities/comment.entity';
 import { CommentRepository } from '@comment/repositories/comment.repository';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeormExceptionFilter } from '@root/filters/typeorm-exception.filter';
 import { ReactionModule } from '@root/reaction/reaction.module';
 import { ReactionArticleRepository } from '@root/reaction/repositories/reaction-article.repository';
+import { UpdateUserProfileRequestDto } from '@root/user/dto/request/update-user-profile-request.dto';
 import { UserRole } from '@root/user/interfaces/userrole.interface';
-import { clearDB } from '@test/e2e/utils/utils';
 import * as dummy from '@test/e2e/utils/dummy';
+import { clearDB } from '@test/e2e/utils/utils';
+import { UserRepository } from '@user/repositories/user.repository';
+import { UserModule } from '@user/user.module';
+import * as cookieParser from 'cookie-parser';
+import * as request from 'supertest';
+import { getConnection } from 'typeorm';
+import { TestBaseModule } from './test.base.module';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -137,18 +136,26 @@ describe('UserController (e2e)', () => {
       expect(response.body.id).toEqual(user.id);
       expect(response.body.nickname).toEqual(user.nickname);
     });
+
+    test('[실패] GET - 없는 유저 id를 요청한 경우', async () => {
+      const response = await request(app)
+        .get(`/users/${999}`)
+        .set('Cookie', `${process.env.ACCESS_TOKEN_KEY}=${JWT}`);
+
+      // 404를 던지는게 원래 맞는 건가요
+      // TODO - 스웨거에 반영하기
+      expect(response.status).toEqual(404);
+    });
   });
 
   describe('/users', () => {
     let user;
+    const githubUid = 'test github uid';
+    const nickname = 'test nickname';
+    const githubUsername = 'github user name';
 
     beforeEach(async () => {
-      user = dummy.user(
-        'test1234',
-        'first user',
-        'githubUsername',
-        UserRole.CADET,
-      );
+      user = dummy.user(githubUid, nickname, githubUsername, UserRole.CADET);
       await userRepository.save(user);
       JWT = authService.getJWT({
         userId: user.id,
@@ -161,7 +168,6 @@ describe('UserController (e2e)', () => {
         nickname: 'rockpell',
         character: 2,
       } as UpdateUserProfileRequestDto;
-
       const response = await request(app)
         .put('/users')
         .send(updateData)
@@ -172,6 +178,74 @@ describe('UserController (e2e)', () => {
       const updatedUser = await userRepository.findOne(user.id);
       expect(updatedUser.nickname).toEqual('rockpell');
       expect(updatedUser.character).toEqual(2);
+    });
+
+    // TODO - 이게 올바른 동작인지 확인
+    test('[성공] PUT - 닉네임 길이가 0인 경우', async () => {
+      const updateData = {
+        nickname: '',
+        character: 2,
+      } as UpdateUserProfileRequestDto;
+      const response = await request(app)
+        .put('/users')
+        .send(updateData)
+        .set('Cookie', `${process.env.ACCESS_TOKEN_KEY}=${JWT}`);
+
+      expect(response.status).toEqual(200);
+
+      const updatedUser = await userRepository.findOne(user.id);
+      expect(updatedUser.nickname).toEqual('');
+      expect(updatedUser.character).toEqual(2);
+    });
+
+    test('[성공] PUT - 닉네임만 변경하는 경우', async () => {
+      const updateData = {
+        nickname: 'mimseong',
+      } as UpdateUserProfileRequestDto;
+      const response = await request(app)
+        .put('/users')
+        .send(updateData)
+        .set('Cookie', `${process.env.ACCESS_TOKEN_KEY}=${JWT}`);
+
+      expect(response.status).toEqual(200);
+
+      const updatedUser = await userRepository.findOne(user.id);
+      expect(updatedUser.nickname).toEqual('mimseong');
+      expect(updatedUser.character).toEqual(0);
+    });
+
+    test('[성공] PUT - 캐릭터만 변경하는 경우', async () => {
+      const updateData = {
+        character: 2,
+      } as UpdateUserProfileRequestDto;
+      const response = await request(app)
+        .put('/users')
+        .send(updateData)
+        .set('Cookie', `${process.env.ACCESS_TOKEN_KEY}=${JWT}`);
+
+      expect(response.status).toEqual(200);
+
+      const updatedUser = await userRepository.findOne(user.id);
+      expect(updatedUser.nickname).toEqual(nickname);
+      expect(updatedUser.character).toEqual(2);
+    });
+
+    test('[실패] PUT - 없는 캐릭터 번호', async () => {
+      const updateData = {
+        nickname: 'rockpell',
+        character: 999,
+      } as UpdateUserProfileRequestDto;
+      const response = await request(app)
+        .put('/users')
+        .send(updateData)
+        .set('Cookie', `${process.env.ACCESS_TOKEN_KEY}=${JWT}`);
+
+      // TODO - 스웨거에 반영하기
+      expect(response.status).toEqual(400);
+
+      const updatedUser = await userRepository.findOne(user.id);
+      expect(updatedUser.nickname).toEqual(nickname);
+      expect(updatedUser.character).toEqual(0);
     });
 
     test('[성공] DELETE - 유저 삭제하기', async () => {
