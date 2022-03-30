47ecbf1697fe72506b8ece7c701055d4b62d9496
rockpell <aythffk@gmail.com>
2022-03-30 13:06:52 +0900

    Test: createTestApp util 추가
    - test 코드에서 nest app을 생성하고 filter, pipe, cookierParser를 적용하는 코드가 반복되서 별도의 함수로 분리

--- test/e2e/utils/utils.ts
+++ test/e2e/utils/utils.ts
@@ -1,4 +1,10 @@
 import { getConnection } from 'typeorm';
+import * as cookieParser from 'cookie-parser';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { TestingModule } from '@nestjs/testing';
+
+import { InternalServerErrorExceptionFilter } from '@root/filters/internal-server-error-exception.filter';
+import { TypeormExceptionFilter } from '@root/filters/typeorm-exception.filter';
 
 export const clearDB = async () => {
   const entities = getConnection().entityMetadatas;
@@ -6,4 +12,22 @@ export const clearDB = async () => {
     const repository = getConnection().getRepository(entity.name);
     await repository.query(`TRUNCATE TABLE ${entity.tableName}`);
   }
+};
+
+export const createTestApp = (
+  moduleFixture: TestingModule,
+): INestApplication => {
+  const app = moduleFixture.createNestApplication();
+
+  app.use(cookieParser());
+  app.useGlobalFilters(new InternalServerErrorExceptionFilter());
+  app.useGlobalFilters(new TypeormExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  return app;
 };
