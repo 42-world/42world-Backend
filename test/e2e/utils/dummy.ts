83c0bebaa4bc0e69bc3197bb54e07a2de11847e8
huni <petercha2000@gmail.com>
2022-03-31 18:20:13 +0900

    Feat: 공통적으로 쓰일 유저와 카테고리 더미 데이터 추가
    - 공동적으로 쓰일수있는 권한 별 유저 더미 추가
    - 카테고리 종류별 더미 데이터 추가
    - 유저를 통해서 jwt를 만드는 jwt2 추가됨

--- test/e2e/utils/dummy.ts
+++ test/e2e/utils/dummy.ts
@@ -6,6 +6,8 @@ import { Comment } from '@comment/entities/comment.entity';
 import { UserRole } from '@root/user/interfaces/userrole.interface';
 import { User } from '@user/entities/user.entity';
 import { ReactionArticle } from '@root/reaction/entities/reaction-article.entity';
+import { UserRepository } from '@root/user/repositories/user.repository';
+import { CategoryRepository } from '@root/category/repositories/category.repository';
 
 export const user = (
   githubUid: string,
@@ -32,9 +34,19 @@ export const jwt = (
   } as JWTPayload);
 };
 
-export const category = (name: string) => {
+export const category = (
+  name: string,
+  commonRole: UserRole = UserRole.CADET,
+  anonymity = false,
+) => {
   const category = new Category();
   category.name = name;
+  category.readableArticle = commonRole;
+  category.writableArticle = commonRole;
+  category.readableComment = commonRole;
+  category.writableComment = commonRole;
+  category.reactionable = commonRole;
+  category.anonymity = anonymity;
   return category;
 };
 
@@ -72,4 +84,66 @@ export const reactionArticle = (
   reactionArticle.articleId = articleId;
   reactionArticle.userId = userId;
   return reactionArticle;
+};
+
+export type DummyUsers = {
+  cadet: User[];
+  admin: User[];
+  novice: User[];
+};
+
+export const createDummyUsers = async (
+  userRepository: UserRepository,
+): Promise<DummyUsers> => {
+  const dummyUsers: DummyUsers = {
+    cadet: await userRepository.save([
+      user('uid1', 'name1', 'githubName1', UserRole.CADET),
+      user('uid2', 'name2', 'githubName2', UserRole.CADET),
+    ]),
+    admin: await userRepository.save([
+      user('uid3', 'name3', 'githubName3', UserRole.ADMIN),
+      user('uid4', 'name4', 'githubName4', UserRole.ADMIN),
+    ]),
+    novice: await userRepository.save([
+      user('uid5', 'name5', 'githubName5', UserRole.NOVICE),
+      user('uid6', 'name6', 'githubName6', UserRole.NOVICE),
+    ]),
+  };
+
+  return dummyUsers;
+};
+
+export type DummyCategories = {
+  free: Category;
+  notice: Category;
+  forall: Category;
+  anony: Category;
+};
+
+export const createDummyCategories = async (
+  categoryRepository: CategoryRepository,
+): Promise<DummyCategories> => {
+  const dummyCategories: DummyCategories = {
+    free: await categoryRepository.save(
+      category('free category', UserRole.CADET),
+    ),
+    notice: await categoryRepository.save(
+      category('notice category', UserRole.ADMIN),
+    ),
+    forall: await categoryRepository.save(
+      category('forall category', UserRole.NOVICE),
+    ),
+    anony: await categoryRepository.save(
+      category('anony category', UserRole.CADET, true),
+    ),
+  };
+
+  return dummyCategories;
+};
+
+export const jwt2 = (user: User, authService: AuthService): string => {
+  return authService.getJWT({
+    userId: user.id,
+    userRole: user.role,
+  } as JWTPayload);
 };
