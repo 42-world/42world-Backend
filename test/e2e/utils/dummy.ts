40cb9877ae27114d7df06af7cff599dfb55c5efa
siontama <siontama@naver.com>
2022-04-01 16:30:21 +0900

    Test: notification dummy 인자 추가

--- test/e2e/utils/dummy.ts
+++ test/e2e/utils/dummy.ts
@@ -4,6 +4,7 @@ import { JWTPayload } from '@auth/interfaces/jwt-payload.interface';
 import { Category } from '@category/entities/category.entity';
 import { Comment } from '@comment/entities/comment.entity';
 import { Notification } from '@root/notification/entities/notification.entity';
+import { NotificationType } from '@root/notification/interfaces/notifiaction.interface';
 import { ReactionArticle } from '@root/reaction/entities/reaction-article.entity';
 import { UserRole } from '@root/user/interfaces/userrole.interface';
 import { User } from '@user/entities/user.entity';
@@ -76,11 +77,13 @@ export const reactionArticle = (
 };
 
 export const notification = (
+  type: NotificationType,
   userId: number,
   articleId: number,
   content: string,
 ): Notification => {
   const notification = new Notification();
+  notification.type = type;
   notification.content = content;
   notification.userId = userId;
   notification.articleId = articleId;
