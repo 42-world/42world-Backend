1c4947a2317de1e4bf8c5156107b0d9203799363
huni <petercha2000@gmail.com>
2022-04-07 02:51:05 +0900

    Refactor: wait-for-healthy 함수화
    - healthy check 함수로 뺌

--- wait-for-healthy.sh
+++ wait-for-healthy.sh
@@ -2,17 +2,24 @@
 
 container_name=$1
 
+is_db_healthy() {
+    test "$( docker container inspect -f '{{.State.Health.Status}}' $container_name )" == "healthy"
+}
+
 wait_for()
 {
+    if is_db_healthy; then
+        return 0
+    fi
     while :
     do
-        if [ "$( docker container inspect -f '{{.State.Health.Status}}' $container_name )" == "healthy" ]; then
+        echo "waiting for healthy db..."
+        if is_db_healthy; then
             sleep 5
             echo "waiting done!"
             break
         fi
         sleep 5
-        echo "waiting for healthy db..."
     done
 }
 
