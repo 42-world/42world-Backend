9a6d70c4751b6f0b5fc915e52a0d6d7a0775c8be
SeongMin Park <parkseongmin98@gmail.com>
2022-04-18 21:15:26 +0900

    Refactor: 예외 필터 정리

--- apps/api/src/ft-checkin/ft-checkin.service.ts
+++ apps/api/src/ft-checkin/ft-checkin.service.ts
@@ -1,4 +1,5 @@
 import { CacheService } from '@app/common/cache/cache.service';
+import { logger } from '@app/utils/logger';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 import axios from 'axios';
@@ -40,7 +41,7 @@ export class FtCheckinService {
         });
         return realData;
       } catch (e) {
-        console.error(e);
+        logger.error(e);
         throw new NotFoundException('데이터를 받아올 수 없습니다.');
       }
     }
