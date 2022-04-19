b5a1e2b31ce7f60ebdf2ced7822aa2aa3b29a3ec
huni <petercha2000@gmail.com>
2022-04-20 00:36:12 +0900

    Feat: 메세지 필터 기능 직접 구현

--- apps/api/src/slack/slack.controller.ts
+++ apps/api/src/slack/slack.controller.ts
@@ -1,33 +1,20 @@
 import { Public } from '@api/auth/auth.decorator';
-import { logger } from '@app/utils/logger';
 import { Body, Controller, Post } from '@nestjs/common';
-import {
-  IncomingSlackEvent,
-  MessageEvent,
-  SlackEventHandler,
-  SlackEventListener,
-} from 'nestjs-slack-listener';
+import { IncomingSlackEvent, MessageEvent } from 'nestjs-slack-listener';
 import { SlackService } from './slack.service';
 
-const FTWORLD_RANDOM = 'C03159JLAV7';
-
 @Controller('slack')
-@SlackEventListener()
 export class SlackController {
   constructor(private readonly slackService: SlackService) {}
 
   @Post()
   @Public()
-  @SlackEventHandler({
-    eventType: 'message' || 'message_changed',
-    filter: ({ event }) => event.channel === FTWORLD_RANDOM,
-  })
   event(
     @Body() { event, challenge }: IncomingSlackEvent<MessageEvent>,
   ): string | void {
     if (challenge) {
       return challenge;
     }
-    logger.info(event.text);
+    this.slackService.handleMessageEvent(event);
   }
 }
