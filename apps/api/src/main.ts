a44776796e0b4c857302f0a2ec69626f7a45e610
raejun92 <cvcv1324@naver.com>
2022-04-20 06:55:36 +0900

    feat: sentry 연결

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,15 +7,21 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
 import * as cookieParser from 'cookie-parser';
 import * as morgan from 'morgan';
 import { join } from 'path';
+import { SentryInterceptor } from './sentry.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
+  Sentry.init({
+    dsn: 'https://daf619595fcf4a259488d1cb4afd124f@o1210892.ingest.sentry.io/6346771',
+  });
+  app.useGlobalInterceptors(new SentryInterceptor());
   morgan.token('body', (req) => JSON.stringify(req.body));
   app.use(
     morgan(
