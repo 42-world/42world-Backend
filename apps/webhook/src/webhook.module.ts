e7ebf0b9f2a67878270b53d9e35df72ebcbcf451
huni <petercha2000@gmail.com>
2022-04-20 02:03:23 +0900

    Feat: WEBHOOK_EXTERNAL_PORT env 추가됨

--- apps/webhook/src/webhook.module.ts
+++ apps/webhook/src/webhook.module.ts
@@ -1,10 +1,20 @@
+import { ormconfig } from '@app/common/database/ormconfig';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SlackModule } from './slack/slack.module';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
-import { SlackModule } from './slack/slack.module';
 
 @Module({
-  imports: [SlackModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'config/.env',
+      isGlobal: true,
+      cache: true,
+      load: [ormconfig],
+    }),
+    SlackModule,
+  ],
   controllers: [WebhookController],
   providers: [WebhookService],
 })
