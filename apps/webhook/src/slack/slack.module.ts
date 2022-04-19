034938995188cf1b819789f7224482cbb9516a86
huni <petercha2000@gmail.com>
2022-04-20 03:28:01 +0900

    Feat: slack app 에서 db 접속 추가
    - slack 레파지토리 추가

--- apps/webhook/src/slack/slack.module.ts
+++ apps/webhook/src/slack/slack.module.ts
@@ -1,10 +1,12 @@
+import { Slack } from '@app/entity/slack/slack.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SlackHandlerModule } from 'nestjs-slack-listener';
 import { SlackController } from './slack.controller';
 import { SlackService } from './slack.service';
 
 @Module({
-  imports: [SlackHandlerModule.forRoot({})],
+  imports: [SlackHandlerModule.forRoot({}), TypeOrmModule.forFeature([Slack])],
   controllers: [SlackController],
   providers: [SlackService],
 })
