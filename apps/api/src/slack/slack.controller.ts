6b0a75fb4e6c148237598f6520b5d9db111fea06
huni <petercha2000@gmail.com>
2022-04-19 19:50:49 +0900

    Feat: nestjs-slack-listener 추가 및 적용

--- apps/api/src/slack/slack.controller.ts
+++ apps/api/src/slack/slack.controller.ts
@@ -1,9 +1,18 @@
 import { Public } from '@api/auth/auth.decorator';
 import { logger } from '@app/utils/logger';
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  IncomingSlackEvent,
+  MessageEvent,
+  SlackEventHandler,
+  SlackEventListener,
+} from 'nestjs-slack-listener';
 import { SlackService } from './slack.service';
 
+const SHOUT_CHANNEL = 'C02LK2VTAQ4';
+
 @Controller('slack')
+@SlackEventListener(({ event }) => event.channel === SHOUT_CHANNEL)
 export class SlackController {
   constructor(private readonly slackService: SlackService) {}
 
@@ -15,7 +24,10 @@ export class SlackController {
 
   @Get()
   @Public()
-  event(@Body() payload: string): void {
-    logger.info(payload);
+  @SlackEventHandler({
+    eventType: 'message',
+  })
+  event(@Body() { event: { text } }: IncomingSlackEvent<MessageEvent>): void {
+    logger.info(text);
   }
 }
