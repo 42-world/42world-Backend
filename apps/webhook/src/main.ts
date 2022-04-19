cb928e7cea93635b9419bc0ad1feab6501e0bc66
huni <petercha2000@gmail.com>
2022-04-20 02:03:23 +0900

    Feat: WEBHOOK_EXTERNAL_PORT env 추가됨

--- apps/webhook/src/main.ts
+++ apps/webhook/src/main.ts
@@ -1,10 +1,13 @@
 import { stream } from '@app/utils/logger';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as morgan from 'morgan';
 import { WebhookModule } from './webhook.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(WebhookModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('WEBHOOK_EXTERNAL_PORT');
 
   morgan.token('body', (req) => JSON.stringify(req.body));
   app.use(
@@ -14,6 +17,6 @@ async function bootstrap() {
     ),
   );
 
-  await app.listen(3000);
+  await app.listen(port || 3000);
 }
 bootstrap();
