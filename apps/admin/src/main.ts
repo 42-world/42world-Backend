83331a8a62baeefba08514285497550951156707
rockpell <aythffk@gmail.com>
2022-04-20 02:14:45 +0900

    Feat: admin port, env로 변경

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,9 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AdminModule } from './admin.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AdminModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('ADMIN_PORT');
+  await app.listen(port);
 }
 bootstrap();
