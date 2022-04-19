8951a3c9d7dc6a9b3d716b4aeaf5cd12df72f262
huni <petercha2000@gmail.com>
2022-04-19 20:07:21 +0900

    FIx: 슬랙봇 토큰 env 화

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -72,7 +72,7 @@ import { SlackModule } from './slack/slack.module';
     }),
     SlackHandlerModule.forRootAsync({
       useFactory: async () => ({
-        botToken: 'nonononono',
+        botToken: 'process.env.SLACK_BOT_TOKEN',
       }),
     }),
     CommentModule,
