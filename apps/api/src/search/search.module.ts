981729064015239a4ebbf461c6b2ae974ec45a03
siontama <siontama@naver.com>
2022-05-09 14:44:10 +0900

    Refactor: Search-domain Article로 통합

--- apps/api/src/search/search.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SearchController } from './search.controller';
-
-@Module({
-  controllers: [SearchController],
-  providers: [],
-  exports: [],
-})
-export class SearchModule {}
