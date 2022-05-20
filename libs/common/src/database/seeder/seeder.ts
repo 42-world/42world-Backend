609c56bca3f2f497ac4eb2b7280a67d077cdd696
rockpell <aythffk@gmail.com>
2022-05-21 01:29:37 +0900

    Feat: user seed data의 권한에 맞게 intra-auth도 데이터 생성
    - cadet, admin 계정은 intra-auth 테이블에도 데이터가 있어야하기 때문에 시드 데이터 추가

--- libs/common/src/database/seeder/seeder.ts
+++ libs/common/src/database/seeder/seeder.ts
@@ -1,3 +1,4 @@
+import { IntraAuthSeederService } from '@app/common/database/seeder/intra-auth/intra-auth-seeder.service';
 import { Injectable } from '@nestjs/common';
 import { ArticleSeederService } from './article/article-seeder.service';
 import { CategorySeederService } from './category/category-seeder.service';
@@ -9,6 +10,7 @@ export class Seeder {
     private readonly userSeederService: UserSeederService,
     private readonly categorySeederService: CategorySeederService,
     private readonly articleSeederService: ArticleSeederService,
+    private readonly intraAuthSeederService: IntraAuthSeederService,
   ) {}
 
   async seed() {
@@ -16,6 +18,7 @@ export class Seeder {
       this.userSeederService.create(),
       this.categorySeederService.create(),
       this.articleSeederService.create(),
+      this.intraAuthSeederService.create(),
     ]);
   }
 }
