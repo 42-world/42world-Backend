9085329bb4cdcf9da7cba79b42baed1806117c2a
rockpell <aythffk@gmail.com>
2022-06-05 14:08:32 +0900

    Fix: prettier 설정이 적용이 안되던 문제 수정
    - eslint rules에 'prettier/prettier' 옵션을 설정하면 prettierrc 파일을 읽지 않기 때문에 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -21,6 +18,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ["error", { "endOfLine": "auto" }],
   },
 };
