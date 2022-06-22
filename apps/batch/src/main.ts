01ea2cc4824f9738cda8de16ce40de17d9b20ee9
huni <petercha2000@gmail.com>
2022-06-22 15:10:53 +0900

    Feat: 앱 시작시 api 호출 추가
    - 인스턴스가 처음 뜨고나서 5분 이내로 값을 가져오지 않아 오류가 발생함
    - 처음 뜨자마자 한번은 값을 가져오도록 변경

--- apps/batch/src/main.ts
+++ apps/batch/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { BatchModule } from './batch.module';
+import { FtCheckinService } from './ft-checkin/ft-checkin.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(BatchModule);
   app.useGlobalFilters(); // TODO: 필터 적용할것
+
+  const ftcheckinService = app.get(FtCheckinService);
+
+  ftcheckinService.getMax();
+  ftcheckinService.getNow();
+
   await app.listen(3000);
 }
 bootstrap();
