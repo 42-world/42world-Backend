cee5df265606effb7674dcc95679b9df8916b592
raejun92 <cvcv1324@naver.com>
2022-06-27 01:15:19 +0900

    fix: batch에 sentry init 적용

--- apps/batch/src/main.ts
+++ apps/batch/src/main.ts
@@ -1,14 +1,19 @@
 import { SentryInterceptor } from '@app/common/interceptor/sentry.interceptor';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
 import { BatchModule } from './batch.module';
 import { FtCheckinService } from './ft-checkin/ft-checkin.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(BatchModule);
-  app.useGlobalInterceptors(new SentryInterceptor());
-
+  const configService = app.get(ConfigService);
   const ftcheckinService = app.get(FtCheckinService);
 
+  Sentry.init({
+    dsn: configService.get('SENTRY_KEY'),
+  });
+  app.useGlobalInterceptors(new SentryInterceptor());
   ftcheckinService.getMax();
   ftcheckinService.getNow();
 
