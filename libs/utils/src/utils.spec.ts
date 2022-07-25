4f2d3ee4742dcfff24d8e0d4cd0530372f7e12a5
rockpell <aythffk@gmail.com>
2022-07-25 21:48:13 +0900

    Test: Guest role이 추가되면서 실패하던 유닛테스트 수정

--- libs/utils/src/utils.spec.ts
+++ libs/utils/src/utils.spec.ts
@@ -5,18 +5,18 @@ describe('includeRole', () => {
   test('ADMIN 권한을 확인하는 경우', () => {
     const result = includeRole(UserRole.ADMIN);
 
-    expect(result.sort()).toStrictEqual([UserRole.ADMIN, UserRole.CADET, UserRole.NOVICE]);
+    expect(result.sort()).toStrictEqual([UserRole.ADMIN, UserRole.CADET, UserRole.GUEST, UserRole.NOVICE]);
   });
 
   test('CADET 권한을 확인하는 경우', () => {
     const result = includeRole(UserRole.CADET);
 
-    expect(result.sort()).toStrictEqual([UserRole.CADET, UserRole.NOVICE]);
+    expect(result.sort()).toStrictEqual([UserRole.CADET, UserRole.GUEST, UserRole.NOVICE]);
   });
 
   test('NOVICE 권한을 확인하는 경우', () => {
     const result = includeRole(UserRole.NOVICE);
 
-    expect(result.sort()).toStrictEqual([UserRole.NOVICE]);
+    expect(result.sort()).toStrictEqual([UserRole.GUEST, UserRole.NOVICE]);
   });
 });
