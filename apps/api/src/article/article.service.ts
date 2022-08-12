bc7409d9195e0e91bddd636daa13c01c52120980
siontama <siontama@naver.com>
2022-08-12 18:47:44 +0900

    Fix: exclude img from search

--- apps/api/src/article/article.service.ts
+++ apps/api/src/article/article.service.ts
@@ -66,6 +66,15 @@ export class ArticleService {
       categoryIds = [options.categoryId];
     }
     const { articles, totalCount } = await this.articleRepository.search(options, categoryIds);
+    for (let i = 0; i < articles.length; i++) {
+      if (
+        articles[i].content.replace(/<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>/g, '').indexOf(options.q) === -1 &&
+        articles[i].title.indexOf(options.q) === -1
+      ) {
+        articles.splice(i, 1);
+        i--;
+      }
+    }
     return { articles, totalCount };
   }
 
