57ffce970cc84870a3cc293e9451b506cbdd43d4
huni <petercha2000@gmail.com>
2022-09-05 06:14:15 +0900

    test: github-auth 테스트 추가

---
+++ apps/api/src/auth/github-auth/__test__/github-auth.module.test.ts
@@ -0,0 +1,25 @@
+import { ConfigModule } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import { GithubAuthModule } from '../github-auth.module';
+
+describe('GithubAuthModule', () => {
+  it('모듈이 잘 컴파일된다.', async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [
+            () => ({
+              GITHUB_CLIENT_ID: '123',
+              GITHUB_CLIENT_SECRET: '123',
+              GITHUB_CALLBACK_URL: '123',
+            }),
+          ],
+        }),
+        GithubAuthModule,
+      ],
+    }).compile();
+
+    expect(module).toBeDefined();
+  });
+});
