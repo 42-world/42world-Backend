1cf24d278644f7476610b9c952e97d6c81104e4e
huni <petercha2000@gmail.com>
2022-10-22 05:00:52 +0900

    refactor: article dto 뜯어고침

---
+++ apps/api/src/article/dto/response/__test__/article-response.dto.test.ts
@@ -0,0 +1,75 @@
+import { ArticleResponseDto } from '@api/article/dto/response/article-response.dto';
+import { CategoryResponseDto } from '@api/category/dto/response/category-response.dto';
+import { UserResponseDto } from '@api/user/dto/response/user-response.dto';
+import { UserRole } from '@app/entity/user/interfaces/userrole.interface';
+
+describe('ArticleResponseDto', () => {
+  it('Dto 가 잘 생성된다.', async () => {
+    const articleResponseDto: ArticleResponseDto = new ArticleResponseDto({
+      id: 1,
+      title: '제목 입니다.',
+      content: '내용 입니다.',
+      viewCount: 1,
+      categoryId: 1,
+      category: new CategoryResponseDto({
+        id: 1,
+        name: '카테고리 이름 입니다.',
+        isArticleReadable: true,
+        isArticleWritable: true,
+        isCommentReadable: true,
+        isCommentWritable: true,
+        isAnonymous: true,
+        isReactionable: true,
+      }),
+      writerId: 1,
+      writer: new UserResponseDto({
+        id: 1,
+        nickname: '닉네임 입니다.',
+        role: UserRole.CADET,
+        character: 0,
+        createdAt: new Date('2022-01-01'),
+        updatedAt: new Date('2022-01-01'),
+      }),
+      commentCount: 1,
+      likeCount: 1,
+      createdAt: new Date('2022-01-01'),
+      updatedAt: new Date('2022-01-01'),
+      isSelf: false,
+      isLike: false,
+    });
+
+    expect(articleResponseDto).toMatchInlineSnapshot(`
+      ArticleResponseDto {
+        "category": CategoryResponseDto {
+          "id": 1,
+          "isAnonymous": true,
+          "isArticleReadable": true,
+          "isArticleWritable": true,
+          "isCommentReadable": true,
+          "isCommentWritable": true,
+          "isReactionable": true,
+          "name": "카테고리 이름 입니다.",
+        },
+        "categoryId": 1,
+        "commentCount": 1,
+        "content": "내용 입니다.",
+        "createdAt": 2022-01-01T00:00:00.000Z,
+        "id": 1,
+        "isSelf": false,
+        "likeCount": 1,
+        "title": "제목 입니다.",
+        "updatedAt": 2022-01-01T00:00:00.000Z,
+        "viewCount": 1,
+        "writer": UserResponseDto {
+          "character": 0,
+          "createdAt": 2022-01-01T00:00:00.000Z,
+          "id": 1,
+          "nickname": "닉네임 입니다.",
+          "role": "CADET",
+          "updatedAt": 2022-01-01T00:00:00.000Z,
+        },
+        "writerId": 1,
+      }
+    `);
+  });
+});
--- apps/api/src/article/dto/response/article-response.dto.ts
+++ apps/api/src/article/dto/response/article-response.dto.ts
@@ -1,28 +1,51 @@
-import { BaseArticleDto } from '@api/article/dto/base-article.dto';
 import { CategoryResponseDto } from '@api/category/dto/response/category-response.dto';
-import { AnonyUserResponseDto } from '@api/user/dto/response/anony-user-response.dto';
 import { UserResponseDto } from '@api/user/dto/response/user-response.dto';
-import { ANONY_USER_ID } from '@api/user/user.constant';
-import { Article } from '@app/entity/article/article.entity';
-import { Category } from '@app/entity/category/category.entity';
-import { User } from '@app/entity/user/user.entity';
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class ArticleResponseDto extends PickType(BaseArticleDto, [
-  'id',
-  'title',
-  'content',
-  'viewCount',
-  'categoryId',
-  'category',
-  'writerId',
-  'writer',
-  'commentCount',
-  'likeCount',
-  'createdAt',
-  'updatedAt',
-]) {
-  constructor(config: {
+export class ArticleResponseDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty({ example: '제목 입니다.' })
+  title: string;
+
+  @ApiProperty({ example: '내용 입니다.' })
+  content: string;
+
+  @ApiProperty()
+  viewCount: number;
+
+  @ApiProperty({ example: 1 })
+  categoryId: number;
+
+  @ApiProperty({ type: CategoryResponseDto })
+  category: CategoryResponseDto;
+
+  @ApiProperty()
+  writerId: number;
+
+  @ApiProperty({ type: UserResponseDto })
+  writer: UserResponseDto;
+
+  @ApiProperty()
+  commentCount: number;
+
+  @ApiProperty()
+  likeCount: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty({ example: false })
+  isSelf: boolean;
+
+  @ApiProperty({ required: false, example: false, nullable: true })
+  isLike?: boolean;
+
+  constructor(property: {
     id: number;
     title: string;
     content: string;
@@ -30,53 +53,29 @@ export class ArticleResponseDto extends PickType(BaseArticleDto, [
     categoryId: number;
     category: CategoryResponseDto;
     writerId: number;
-    writer: UserResponseDto | AnonyUserResponseDto;
+    writer: UserResponseDto;
     commentCount: number;
     likeCount: number;
     createdAt: Date;
     updatedAt: Date;
+    isSelf: boolean;
+    isLike: boolean | undefined;
   }) {
-    super();
-
-    this.id = config.id;
-    this.title = config.title;
-    this.content = config.content;
-    this.viewCount = config.viewCount;
-    this.categoryId = config.categoryId;
-    this.category = config.category;
-    this.writerId = config.writerId;
-    this.writer = config.writer;
-    this.commentCount = config.commentCount;
-    this.likeCount = config.likeCount;
-    this.createdAt = config.createdAt;
-    this.updatedAt = config.updatedAt;
-  }
-
-  static of(config: { article: Article; category: Category; writer: User; user: User }): ArticleResponseDto {
-    const category = CategoryResponseDto.of({
-      category: config.category,
-      user: config.user,
-    });
-    const writer = category.isAnonymous ? AnonyUserResponseDto.of() : UserResponseDto.of({ user: config.writer });
-    const writerId = category.isAnonymous ? ANONY_USER_ID : config.article.writerId;
-
-    return new ArticleResponseDto({
-      ...config.article,
-      ...config,
-      category,
-      writer,
-      writerId,
-    });
-  }
-
-  static ofArray(config: { articles: Article[]; user: User }): ArticleResponseDto[] {
-    return config.articles.map((article) =>
-      ArticleResponseDto.of({
-        article,
-        category: article.category,
-        writer: article.writer,
-        user: config.user,
-      }),
-    );
+    this.id = property.id;
+    this.title = property.title;
+    this.content = property.content;
+    this.viewCount = property.viewCount;
+    this.categoryId = property.categoryId;
+    this.category = property.category;
+    this.writerId = property.writerId;
+    this.writer = property.writer;
+    this.commentCount = property.commentCount;
+    this.likeCount = property.likeCount;
+    this.createdAt = property.createdAt;
+    this.updatedAt = property.updatedAt;
+    this.isSelf = property.isSelf;
+    if (property.isLike) {
+      this.isLike = property.isLike;
+    }
   }
 }
