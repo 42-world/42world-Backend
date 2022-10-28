499bbf55313da49f85c2590633825cce694a492a
rockpell <aythffk@gmail.com>
2022-10-29 00:36:04 +0900

    Refacotr: comment create, update, request 객체 수정
    - pickType으로 필요한 프로퍼티만 뽑아서 사용하는 방식을 사용하고 있었으나 직관성이 부족해서 수정

--- apps/api/src/comment/dto/request/create-comment-request.dto.ts
+++ apps/api/src/comment/dto/request/create-comment-request.dto.ts
@@ -1,4 +1,16 @@
-import { PickType } from '@nestjs/swagger';
-import { BaseCommentDto } from '../base-comment.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
 
-export class CreateCommentRequestDto extends PickType(BaseCommentDto, ['content', 'articleId']) {}
+export class CreateCommentRequestDto {
+  @IsString()
+  @MaxLength(420)
+  @IsNotEmpty()
+  @ApiProperty({ example: '댓글 입니다.' })
+  content!: string;
+
+  @IsInt()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty({ example: 1 })
+  articleId!: number;
+}
